f647513e87b109d64fc55604bd9a99101b85e166
ahmad aby ayyasi <abiayyasi12@gmail.com>
2023-09-08 20:26:54 +0700

    change photo

--- src/components/cardBio.tsx
+++ src/components/cardBio.tsx
@@ -27,15 +27,10 @@ const CardBio = ({ variant }: { variant: Variants }) => {
       variants={variant}
       className="rounded-lg border border-zinc-200 dark:border-zinc-800 dark:bg-zinc-900 max-w-[312px] w-full h-[134px] p-[27px] flex items-center"
     >
-      <Image src="/img/bio-image.svg" alt="bio-image" width={80} height={80} />
+      <div className="w-[80px] h-[80px] rounded-full overflow-hidden">
+      <Image src="/img/bioimage.JPG" alt="bio-image" width={80} height={80} className="w-full h-full object-cover"/>
+      </div>
       <div className="ml-[24px]">
-        {/* <BioAchieve
-          image="/img/experience.svg"
-          number="1"
-          title="year experience"
-        />
-        <BioAchieve image="/img/project.svg" number="20+" title="Project" />
-        <BioAchieve image="/img/deal.svg" number="0" title="worked" /> */}
         <BioAchieve number="1" title="Year experience">
           <ExperienceIcon
             fill="e"
